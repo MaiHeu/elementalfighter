50c551a480b4792772144799957dbf4687089755
schattennarr <heunisch@posteo.de>
2019-12-10 10:10:24 +0100

    add gitignore

---
+++ DatabaseHandler.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maik
+ * Date: 10.12.2019
+ * Time: 09:58
+ */
+
+//Konstanten für die Datenbank Verbindung
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "elementalfighter");
+
+function connectToDatabase()
+{
+    try {
+        $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //echo "Connected successfully";
+        return $conn;
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maik Heunisch
+ * Date: 10.12.2019
+ * Time: 09:55
+ */
