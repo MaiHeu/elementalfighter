3ecce839e6f8b573d03c8971827197f02e5be34c
Maik <heunisch@posteo.de>
2020-01-31 20:07:48 +0100

    added isset, funtionality comes later

--- training.php
+++ training.php
@@ -5,15 +5,25 @@
 </head>
 
 <?php
-    include("DatabaseHandler.php");
+include("DatabaseHandler.php");
+$con = connectToDatabase();
 
-    $con = connectToDatabase();
-    $wertnamen = $con->prepare("SELECT `Name`, `Tägl. Training` FROM `Wertnamen`");
-    $wertnamen->execute();
+if (isset($_GET['train'])){
 
-    if(isset($_POST['train'])){
+if(isset($_POST['ckbTraining'])){
 
-    } else {
+}else{
+
+}
+
+
+} else {
+$statement = $con->prepare("SELECT COUNT(*) FROM `Wertnamen`");
+$statement->execute(null);
+$anzahlWerte = $statement->fetch();
+
+$wertnamen = $con->prepare("SELECT `Name`, `Tägl. Training` FROM `Wertnamen`");
+$wertnamen->execute();
 ?>
 <table>
     <tr>
@@ -21,18 +31,21 @@
         <th>Wirkung</th>
         <th>Trainieren</th>
     </tr>
+    <?php foreach ($wertnamen as $wert) { ?>
     <form action="?train=1" method="post">
-        <?php foreach ($wertnamen as $wert) { ?>
             <tr>
                 <td>
-                    <?php    print_r($wert[0]); ?>
+                    <?php print_r($wert[0]); ?>
                 </td>
                 <td>
-                    <?php    print_r($wert[1]." pro Tag"); ?>
+                    <?php print_r($wert[1] . " pro Tag"); ?>
                 </td>
                 <td>
-                    <select name="auswahlTrainingstage"></select> Tag(e) <button type="submit" value="<?php    print_r($wert[0]); ?>" name ="btn_training">Abschicken</button>
-                    <br> Oder bis Wert <input type="text" name="txtTraining" value="0"><input type ="checkbox" name="ckbTraining">
+
+                    <select class="training" name="<?php print_r("auswahlTrainingstage".$wert[0])?>"></select> Tag(e)
+                    <button type="submit" value="<?php print_r($wert[0]); ?>" name="btn_training">Abschicken</button>
+                    <br> Oder bis Wert <input type="text" name="<?php print_r("txtTraining".$wert[0])?>" value="10"><input type="checkbox"
+                                                                                              name="ckbTraining">
                 </td>
             </tr>
         <?php } ?>
@@ -41,9 +54,9 @@
 
 <script language="JavaScript">
     //An dieser Stelle möchte ich betonen, wie sehr ich JavaScript hasse. -Maik, 31.01.2020
-    let select = document.getElementsByName("auswahlTrainingstage");
-    for(let j = 0; j < select.length; j++){
-        for(let i = 1; i <=30; i++){
+    let select = document.getElementsByClassName("training");
+    for (let j = 0; j < select.length; j++) {
+        for (let i = 1; i <= 30; i++) {
             let el = document.createElement("option");
             el.textContent = i.toString();
             el.value = i.toString();
@@ -52,5 +65,5 @@
     }
 </script>
 <?php
-    print_r ($_POST);
+print_r($_POST);
 }
