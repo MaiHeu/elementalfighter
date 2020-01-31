56cc293743514477993cda8abc37a329dcc0fefc
milihamster <hopemili@yahoo.com>
2020-01-31 20:14:50 +0100

    Added background picture and adjusted colors accordingly

--- home.php
+++ home.php
@@ -20,7 +20,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-12">
-			<h2>Charaktere</h2>
+			<h2 style="color: white">Charaktere</h2>
 			<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="0">
 			<!-- Wrapper for carousel items -->
 			<div class="carousel-inner">
